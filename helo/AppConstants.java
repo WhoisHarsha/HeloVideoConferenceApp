d026aaceb55c6ee8eb606f331680fe0ce1e5588c
WhoIsH <88264974+WhoisHarsha@users.noreply.github.com>
2024-06-11 20:34:44 +0530

    Add files via upload

---
+++ helo/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.helo;
+
+public class AppConstants {
+    public static long appID = 201366206;
+    public static String appSign = "08c2f9e21d8d8e390dcf8acbf7a2befaa6e658069363488d5d1bffcd584dc63a";
+}
